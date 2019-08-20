d0112acf1dc97ee6b51a375fdd062e94b5c28d9b
Irene Fernandes <fernandesm.irene@gmail.com>
2019-08-20 16:45:56 +0100

    solution task manager

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_105423) do
+ActiveRecord::Schema.define(version: 2019_08_20_112426) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "to_do_lists", force: :cascade do |t|
     t.string "title"
